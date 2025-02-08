7c2c9df496ef8a170645f91ee9acb45e50228cf2
pchchv <ipchchv@gmail.com>
2025-02-08 17:14:31 +0300

    Introduce UrlIter struct for URL iteration

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use reqwest::Url;
+use scraper::html::Select;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Iterator returns `(String, Url)` pairs per iteration.
+pub struct UrlIter<'a, 'b> {
+    url: &'a Url,
+    data: Select<'a, 'b>
 }
