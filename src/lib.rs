371835fd3f9ba4e08e11807bece4752a305698ee
pchchv <ipchchv@gmail.com>
2025-02-08 17:15:29 +0300

    Implement Iterator for UrlIter struct to enable URL extraction

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,20 @@ pub struct UrlIter<'a, 'b> {
     url: &'a Url,
     data: Select<'a, 'b>
 }
+
+impl<'a, 'b> Iterator for UrlIter<'a, 'b> {
+    type Item = (String, Url);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for element in &mut self.data {
+            if let Some(url) = element.value().attr("href") {
+                if !url.starts_with('?') {
+                    if let Ok(url) = self.url.join(url) {
+                        return Some((element.inner_html(), url));
+                    }
+                }
+            }
+        }
+        None
+    }
+}
